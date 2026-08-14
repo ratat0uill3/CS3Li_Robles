745a4ab394ef163a93c76f703a964547157034e5
IceBerg <roblesih30@wvc.pshs.edu.ph>
2026-08-14 11:10:17 +0800

    Add files via upload

---
+++ diagnostic1_li_Robles.py
@@ -0,0 +1,21 @@
+
+
+def  calculate_checkout(cart_total, shipping_speed):
+    cart_total = float(input("Enter cart total: "))
+    shipping_speed = input("Enter shipping choice (express/overnight/standard): ")
+    if shipping_speed == "express":
+        cart_total = cart_total + 15
+        print("Your shipping fee is $15 and your total is ($): ")
+    elif shipping_speed == "overnight":
+        cart_total = cart_total + 25
+        print("Your shipping fee is $25 and your total is ($): ")
+    elif shipping_speed == "standard":
+        cart_total = cart_total + 10
+        print("Your shipping fee is $10 and your total is ($): ")
+    else:
+        print("Invalid shipping choice.")
+
+    return cart_total
+
+
+print((calculate_checkout(1000,"express"))) 
---
+++ diagnostic2_li_Robles.py
@@ -0,0 +1,28 @@
+
+def calculate_fuel(cargo_weight):
+    total_weight = cargo_weight + 50000
+    fuel = total_weight * 3
+    return fuel
+
+total_cargo_weight = 0
+
+while True:
+    cargos = input("Enter the cargo type satellite/rover/supplies. (Type launch to deploy): ")
+    if cargos == "launch":
+        break
+
+    if cargos == "satellite":
+        total_cargo_weight = total_cargo_weight + 1000
+    elif cargos == "rover":
+        total_cargo_weight = total_cargo_weight + 2500
+    elif cargos == "supplies":
+        total_cargo_weight = total_cargo_weight + 500
+    else:
+        print("Invalid cargo type.")
+
+    if total_cargo_weight > 10000:
+        print("MAX WEIGHTREACHED")
+        break
+
+print("Total cargo weight:", total_cargo_weight, "kg")
+print("Fuel needed:", calculate_fuel(total_cargo_weight), "liters")
